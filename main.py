15753475a18ee742ff97eacc9bcb4219573fb4b3
Mateus Vega <mateusggvega@gmail.com>
2024-02-12 21:41:57 -0300

    Optimize Console
    Optimize Console and put the initial code in a function.

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from time import sleep
-options = webdriver.ChromeOptions()
-options.add_argument("--headless=new")
-driver = webdriver.Chrome(options=options)
-while True:
-    with open("D:\VSprojects\Python\PySearching\websites.txt", "r") as file:
-        for url in file.readlines():
-            driver.get(url)
-            sleep(15)
+import os
+def enter_urls():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('log-level=3')
+    driver = webdriver.Chrome(options=chrome_options)
+    nOpen = 0
+    while True:
+        try:
+            with open("D:\VSprojects\Python\PySearching\websites.txt", "r") as file:
+                for url in file.readlines():
+                    driver.get(url)
+                    nOpen +=1
+                    os.system('cls')
+                    print(f"It is in link {nOpen}")
+                    sleep(3)
+        except:
+            print(end="")
