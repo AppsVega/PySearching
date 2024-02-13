b34859c90d4387a6cac8c0832c886ddde35783eb
Mateus Vega <mateusggvega@gmail.com>
2024-02-13 11:22:48 -0300

    Update PySimpleGUI
    Added on off buttons, added feedback on states and number of links.

--- main.py
+++ main.py
@@ -2,33 +2,62 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 import PySimpleGUI as sg
-chrome_options = Options()
-chrome_options.add_argument('log-level=3')
-driver = webdriver.Chrome(options=chrome_options)
+import threading
+
+stop = False
 nOpen = 0
+running = False
+state = "OFF"
+tempodelay = 0
+sg.theme('SystemDefault')
+
 def enter_urls():
+    global nOpen, running
+    running = True
+    
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument('log-level=3')
+    
+    driver = webdriver.Chrome(options=chrome_options)
+    
     while True:
         try:
             with open("D:\VSprojects\Python\PySearching\websites.txt", "r") as file:
                 for url in file.readlines():
-                    driver.get(url)
-                    nOpen +=1
+                    if stop:
+                        running = False
+                        driver.quit()
+                        return
+                    driver.get(url.strip())
+                    nOpen += 1
                     sleep(3)
-                    print(nOpen)
-        except:
-            print(end="")
-enter_urls()
+        except Exception as e:
+            print(f"Erro: {e}")
+        running = False
+        driver.quit()
+
 layout = [
-    [sg.Button('ON / OFF')],
+    [sg.Text('', key='state')],
+    [sg.Button('ON'), sg.Button('OFF')],
     [sg.Button('EDIT')],
-    [sg.Text('',key='numOpened')]
+    [sg.Text('', key='numOpened')]
 ]
-window = sg.Window('PySearching',layout=layout)
+
+window = sg.Window('PySearching', layout=layout)
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=0)
     if event == sg.WIN_CLOSED:
         break
-    if event == 'ON / OFF':
-        enter_urls()
+    if event == 'ON':
+        state = 'ON'
+        stop = False
+        if not running:
+            threading.Thread(target=enter_urls).start()
+    if event == 'OFF':
+        stop = True
+        nOpen = 0
+        state = "OFF"
     window['numOpened'].update(f"It is in link {nOpen}")
+    window['state'].update(f'O estado atual é {state}')
